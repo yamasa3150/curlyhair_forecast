a0ad74c769deee4c07a2a0fa85f72bbae423d35f
yamasa3150 <itnzsc4b@icloud.com>
2021-12-06 10:32:29 +0900

    仮のliff機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
   post '/callback' => 'line_bot#callback'
