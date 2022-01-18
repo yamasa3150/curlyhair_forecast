14603807aca0bdb6ef975661e7bed7b12944b3dc
yamasa3150 <itnzsc4b@icloud.com>
2022-01-18 20:15:31 +0900

    Fix:rakeタスクとline_bot_controller修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -31,32 +31,32 @@ class LineBotController < ApplicationController
           humidity = doc.elements[xpath + 'humidity'].attributes['value']
           case event.message['text']
           when  /.*(現在の予報).*/
-            if weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp >= 25.to_s && humidity >= 65.to_s
+            if weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp.to_i >= 25 && humidity.to_i >= 65
               push = "一日中暑く、汗や湿気でくせがでやすいです。\nスタイリングもそうですが発汗対策をしっかり行いましょう！"
-            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp >= 25.to_s && humidity < 65.to_s
+            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp.to_i >= 25 && humidity.to_i < 65
               push =  "湿度はそこまで高くありませんが、一日中気温が高いため汗でくせが出てしまいます。\n発汗対策をしっかり行いましょう!"
-            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp < 25.to_s && humidity >= 65.to_s
+            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp.to_i < 25 && humidity.to_i >= 65
               push = "湿気が多いのでくせが出やすくスタイリングが崩れやすいです。\n外出する際はアイロンとヘアスプレーでしっかりスタイリングしましょう!"
-            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp < 25.to_s && humidity < 65.to_s && humidity > 50.to_s
+            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp.to_i < 25 && humidity.to_i < 65 && humidity.to_i > 50
               push =  "今日は髪のうねりが出にくく髪がまとまりやすい天気です！\n思いっきりスタイリングを楽しみましょう。"
-            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp < 25.to_s && humidity <= 50.to_s
+            elsif weather = /.*(clear sky|few clouds|scattered clouds|broken clouds|overcast clouds).*/ && temp.to_i < 25 && humidity.to_i <= 50
               push = "くせは気になりませんが湿度が低く乾燥や静電気で髪の毛が膨張したり摩擦でキューティクルが剥がれて髪の毛がパサパサになりやすいです。\nスタイリングする際は保湿を心がけましょう。"
-            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp >= 25.to_s && humidity >= 65.to_s
+            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp.to_i >= 25 && humidity.to_i >= 65
               push = "汗、雨、湿気で髪がまとまらず、アイロンやヘアスプレーを使ってもすぐ崩れてしまうかもしれません。\nこういう日が続く場合は思い切って縮毛矯正をかけるのもいいいいかも。"
-            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp >= 25.to_s && humidity < 65.to_s 
+            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp.to_i >= 25 && humidity.to_i < 65
               push = "湿気は気になりませんが、汗と雨で前髪などが崩れるかもしれません。\n発汗対策もそうですが、外出する際はアイロンとヘアスプレーでしっかりスタイリングしましょう!"
-            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp < 25.to_s && humidity >= 65.to_s
+            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp.to_i < 25 && humidity.to_i >= 65
               push = "雨、湿気で髪がまとまらず、アイロンやヘアスプレーを使ってもすぐ崩れてしまうかもしれません。\nこういう日が続く場合は思い切って縮毛矯正をかけるのもいいいいかも。"
-            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp < 25.to_s && humidity < 65.to_s && humidity > 50.to_s
+            elsif weather = /.*(rain|thunderstorm|drizzle).*/ && temp.to_i < 25 && humidity.to_i < 65 && humidity.to_i > 50
               push =  "湿気はそこまで気になりませんが雨に当たってヘアスタイルが崩れないようにアイロンとヘアスプレーでしっかりスタイリングしましょう!"
-            elsif weather =  /.*(rain|thunderstorm|drizzle).*/ && temp < 25.to_s && humidity <= 50.to_s
+            elsif weather =  /.*(rain|thunderstorm|drizzle).*/ && temp.to_i < 25 && humidity.to_i <= 50
               push = "乾燥と雨で髪がまとまらない可能性が高いです。\nスタイリングと保湿をしっかり行いましょう"
             elsif weather =  /.*(snow).*/
-              push = "現在地の天気は雪です\u{2744}\n\n現在の気温は#{now_temp}℃です\u{1F321}"
+              push = "現在地の天気は雪です\u{2744}\n\n現在の気温は#{temp}℃です\u{1F321}"
             elsif weather = /.*(fog|mist|Haze).*/
-              push = "現在地では霧が発生しています\u{1F32B}\n\n現在の気温は#{now_temp}℃です\u{1F321}"
+              push = "現在地では霧が発生しています\u{1F32B}\n\n現在の気温は#{temp}℃です\u{1F321}"
             else
-              push = "現在地では何かが発生していますが、\nご自身でお確かめください。\u{1F605}\n\n現在の気温は#{now_temp}℃です\u{1F321}"
+              push = "現在地では何かが発生していますが、\nご自身でお確かめください。\u{1F605}\n\n現在の気温は#{temp}℃です\u{1F321}"
             end
           end
         end
@@ -78,4 +78,5 @@ class LineBotController < ApplicationController
          config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
        }
      end
+
 end
